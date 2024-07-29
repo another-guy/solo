d9434afcea158f90e3f31bd8c05f2be09a32a926
Igor Soloydenko <isoloydenko@ah4r.com>
2024-07-29 12:38:02 -0700

    fix: no need for promise in index.ts

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
 import { createCli } from './src/cli';
 
-async function main(): Promise<void> {
-  createCli()
-    .parse(process.argv);
-}
-
 function runMain(): void {
-  main()
-    .then(() => {
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  try {
+    createCli()
+      .parse(process.argv)
+  } catch (error) {
+    console.error(error);
+  };
 }
 
 runMain();
